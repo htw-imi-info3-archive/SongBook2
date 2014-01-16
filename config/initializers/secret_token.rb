c0926aae6ba6526a6252096e138671f113c5d104
B Kleinen <kleinen@infrastructure.de>
2014-01-16 13:07:49 +0100

    rails new SongBook2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SongBook2::Application.config.secret_key_base = '8ff9466531b1a873860d1b20b6b77225d1f8ec902b9c7fd20c413a10070b8022070677998d25b68e6b6937516d6a76ae9cfbfbf855c2c756d5c9685b2b42ebe2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SongBook2::Application.config.session_store :cookie_store, key: '_SongBook2_session'
